dfa769227726d1ca118038f3a7bab20904a089c5
Sophia <Sophia@172.20.14.130>
2016-05-19 17:09:12 -0600

    Agregue cola de mensajes en capa de mensajeria

--- ProyectoRedes/Messenger/src/main/java/main/Mensajeria.java
+++ ProyectoRedes/Messenger/src/main/java/main/Mensajeria.java
@@ -8,53 +8,46 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JLabel;
-import java.awt.GridLayout;
 import javax.swing.JList;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.nio.channels.Pipe.SinkChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 import java.awt.event.ActionEvent;
-import javax.swing.BoxLayout;
-import javax.swing.DefaultListModel;
-import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
 
-import java.awt.FlowLayout;
 import router.RouterController;
 import java.awt.Font;
 public class Mensajeria {
 
-	private JFrame frmProyectoRoute;
-	private JTable table;
+	private JFrame frmProyectoRouter;
 	private Map<String, String> map  = new HashMap<String, String>();
-	private ArrayList<String> nodos = new ArrayList<String>();
-	private DefaultListModel<String> listModel = new DefaultListModel<String>();
+	private ArrayList<String> nodes = new ArrayList<String>();
 	public static String hostname;
-	public String mensajeEnviado;
-	private String mensajeRecibido;
-	public DefaultTableModel model;
+	public String messageSent;
+	private String messageReceived;
+	public Queue<String> messagesQueue  = new LinkedList<String>();
 	
 	
-	
-	public String getMensajeEnviado() {
-		return mensajeEnviado;
+	public String getMessageSent() {
+		return messageSent;
 	}
 	
-	public void setMensajeRecibido(String mensajeRecibido) {
-		this.mensajeRecibido = mensajeRecibido;
+	public void setMessageReceived(String mensajeRecibido) {
+		this.messageReceived = mensajeRecibido;
 	}
 	
-	public String[] extraerMensaje(){
-		String[] mensajeLineas = mensajeRecibido.split("\n");
+	public String[] parseMessage(String mssgReceived){
+		String[] mensajeLineas = mssgReceived.split("\n");
 		String[] resultadoMensaje = new String[2];
 		String cuerpoMsg = "";
 		for(String linea: mensajeLineas){
@@ -75,6 +68,26 @@ public class Mensajeria {
 		System.out.println("From->: " + resultadoMensaje[0] + "\nCuerpo del mensaje: " + resultadoMensaje[1]);
 		return resultadoMensaje;
 	}
+	
+	public synchronized void addMessageToQueue(String newMessage){
+		messagesQueue.add(newMessage);
+	}
+	
+	
+	private String[] updateTable(String nuevosNodosTabla){
+		map.remove(map.values());
+		String[] nuevaTabla = nuevosNodosTabla.split("\n");
+		for(String linea: nuevaTabla){
+			String[] nodos = linea.split(",");
+			map.put(nodos[0], nodos[1]);
+		}
+		
+		for(String key: map.keySet()){
+			nodes.add(key);
+		}
+		String[] nodosArray = new String[nodes.size()];
+		return nodes.toArray(nodosArray);
+	}
 
 	/**
 	 * Launch the application.
@@ -85,7 +98,7 @@ public class Mensajeria {
 			public void run() {
 				try {
 					Mensajeria window = new Mensajeria();
-					window.frmProyectoRoute.setVisible(true);
+					window.frmProyectoRouter.setVisible(true);
 					
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -107,11 +120,11 @@ public class Mensajeria {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmProyectoRoute = new JFrame();
-		frmProyectoRoute.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
-		frmProyectoRoute.setTitle("Proyecto Router");
-		frmProyectoRoute.setBounds(100, 100, 570, 500);
-		frmProyectoRoute.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmProyectoRouter = new JFrame();
+		frmProyectoRouter.setFont(new Font("Trebuchet MS", Font.PLAIN, 11));
+		frmProyectoRouter.setTitle("Proyecto Router");
+		frmProyectoRouter.setBounds(100, 100, 570, 500);
+		frmProyectoRouter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 		File myNameFile = new File("..\\Routing\\src\\main\\resources\\myname.txt");
@@ -125,9 +138,11 @@ public class Mensajeria {
 		}
 		hostname = Utils.readFile(myNameFile).get(0).trim();
 		
+		
+		
 		/*Estos valores solo los puse de ejemplo en el hashmap de 
-		 * forwarding table para probar que si genera los
-		 * radio buttons a partir de los nodos existentes  */ 
+		 * forwarding table para probar que si muestra en el 
+		 * JList los nodos existentes*/  
 		map.put("A", "B");
 		map.put("C", "D");
 		map.put("E", "F");
@@ -142,7 +157,7 @@ public class Mensajeria {
 		map.put("W", "X");
 		map.put("Y", "Z");
 		
-		mensajeRecibido = "From:REMITENTE"
+		messageReceived = "From:REMITENTE"
 				+ "\nTo:YO"
 				+ "\nMsg:hola"
 				+ "\nque hace"
@@ -150,11 +165,11 @@ public class Mensajeria {
 				+ "\no que hace"
 				+ "\n."
 				+ "\nEOF";
-		frmProyectoRoute.getContentPane().setLayout(new BorderLayout(0, 0));
+		frmProyectoRouter.getContentPane().setLayout(new BorderLayout(0, 0));
 
 		final JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
-		frmProyectoRoute.getContentPane().add(tabbedPane);
+		frmProyectoRouter.getContentPane().add(tabbedPane);
 		
 		JPanel recibidosPanel = new JPanel();
 		tabbedPane.addTab("Recibidos", null, recibidosPanel, null);
@@ -162,18 +177,6 @@ public class Mensajeria {
 		
 		final JPanel enviarPanel = new JPanel();
 		tabbedPane.addTab("Enviar Nuevo", null, enviarPanel, null);
-		
-		//listScroller.setPreferredSize(new Dimension(250, 80));
-		
-
-		for(String key: map.keySet()){
-			//final JRadioButton button1 = new JRadioButton(key);
-			//nodosPanel.add(button1);
-			nodos.add(key);
-			//listModel.addElement(key);
-		}
-		final String[] nodosArray = new String[nodos.size()];
-		nodos.toArray(nodosArray);
 		enviarPanel.setLayout(null);
 		
 		JLabel lblCuerpoDelMensaje_1 = new JLabel("Mensaje:");
@@ -202,10 +205,15 @@ public class Mensajeria {
 		listScroller.setBounds(46, 36, 83, 278);
 		enviarPanel.add(listScroller);
 		
-		//System.out.println(nodosArray.length);
 		
 		
-		final JList nodosList = new JList(nodos.toArray(nodosArray));
+		for(String key: map.keySet()){
+			nodes.add(key);
+		}
+		
+		final String[] nodosArray = new String[nodes.size()];
+//		nodos.toArray(nodosArray);
+		final JList nodosList = new JList(nodes.toArray(nodosArray));
 		nodosList.setFont(new Font("Trebuchet MS", Font.BOLD, 13));
 		nodosList.setBounds(0, 0, 95, 95);
 		nodosList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -230,12 +238,12 @@ public class Mensajeria {
 				String cuerpoMsj = mensajeTextArea.getText();
 				String para = nodosArray[nodosList.getSelectedIndex()];
 				
-				mensajeEnviado = 
+				messageSent = 
 						"From:" + hostname
 						+ "\nTo:"+ para
 						+ "\nMsg:"+ cuerpoMsj + 
 						"\nEOF";
-				System.out.println(mensajeEnviado);
+				System.out.println(messageSent);
 				msjsEnviadosTextArea.setText( msjsEnviadosTextArea.getText() + "Para " + para + ": " + cuerpoMsj + "\n"+
 				"-----------------------------------------------------------------------------\n");
 				/*AQUI LE TENEMOS QUE PASAR mensajeEnviado 
@@ -251,23 +259,10 @@ public class Mensajeria {
 		recibidosLabel.setBounds(25, 30, 217, 14);
 		recibidosPanel.add(recibidosLabel);
 		
-		/*DefaultTableModel dtm = new DefaultTableModel(0,2);
-		table = new JTable(dtm);
-		model = (DefaultTableModel) table.getModel();
-		table.setBounds(35, 55, 622, 155);
-		recibidosPanel.add(table);
-		
-		
-		
-		JLabel lblCuerpoDelMensaje = new JLabel("Cuerpo del Mensaje:");
-		lblCuerpoDelMensaje.setBounds(25, 241, 200, 14);
-		recibidosPanel.add(lblCuerpoDelMensaje);*/
-		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(25, 55, 514, 307);
 		recibidosPanel.add(scrollPane);
-		
-		
+	
 		final JTextArea msgRcbTextArea = new JTextArea();
 		msgRcbTextArea.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
 		msgRcbTextArea.setEditable(false);
@@ -278,15 +273,58 @@ public class Mensajeria {
 		btnMostrarMsjPrueba.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
 		btnMostrarMsjPrueba.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String[] extract = extraerMensaje();
-				msgRcbTextArea.setText( msgRcbTextArea.getText() + "\n" +extract[0] + " dice: " + extract[1] +"\n");
+//				String[] extract = parseMessage();
+//				msgRcbTextArea.setText( msgRcbTextArea.getText() + "\n" +extract[0] + " dice: " + extract[1] +"\n");
 			}
 		});
 		btnMostrarMsjPrueba.setBounds(393, 373, 146, 23);
 		recibidosPanel.add(btnMostrarMsjPrueba);
 		
-		;
+		JPanel ingresoManualPanel = new JPanel();
+		tabbedPane.addTab("Tabla de Forwarding", null, ingresoManualPanel, null);
+		ingresoManualPanel.setLayout(null);
 		
+		JScrollPane forwardScrollPane = new JScrollPane();
+		forwardScrollPane.setBounds(10, 11, 529, 342);
+		ingresoManualPanel.add(forwardScrollPane);
+		
+		final JTextArea tablaForwardTextArea = new JTextArea();
+		tablaForwardTextArea.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+		tablaForwardTextArea.setBounds(0, 0, 4, 22);
+		forwardScrollPane.setViewportView(tablaForwardTextArea);
+		
+		JButton btnIngresarTabla = new JButton("Ingresar Tabla");
+		btnIngresarTabla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(tablaForwardTextArea.getText()!=null){
+					map.remove(map.values());
+					//System.out.println(map.size());
+					String[] nuevaTabla = tablaForwardTextArea.getText().split("\n");
+					for(String linea: nuevaTabla){
+						String[] nodos = linea.split(",");
+						map.put(nodos[0], nodos[1]);
+					}
+				}
+				for(String key: map.keySet()){
+					
+					String value = map.get(key);
+					System.out.println(key + ", " + value);
+				}
+			}
+		});
+		btnIngresarTabla.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
+		btnIngresarTabla.setBounds(416, 377, 123, 23);
+		ingresoManualPanel.add(btnIngresarTabla);
+		
+		messagesQueue.add("From:Sophia\nTo:A\nMsg:hola que hace\nEOF");
+		while(!messagesQueue.isEmpty()){
+			messageReceived = messagesQueue.poll();
+			String[] extract = parseMessage(messageReceived);
+			msgRcbTextArea.setText( msgRcbTextArea.getText() + "\n" +extract[0] + " dice: " + extract[1] +"\n");
+			
+		}
 		
 	}
+
+	
 }
